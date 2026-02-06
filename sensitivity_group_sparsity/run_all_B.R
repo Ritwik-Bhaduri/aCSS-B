cd4768c65991765e2def961d1a1875c383619f3c
aabeshb <132506636+aabeshb@users.noreply.github.com>
2026-02-06 12:25:20 -0600

    Update run_all_B.R

--- sensitivity_group_sparsity/run_all_B.R
+++ sensitivity_group_sparsity/run_all_B.R
@@ -48,4 +48,4 @@ df = do.call(rbind,df_list)
 end_time <- Sys.time()
 print(end_time-start_time)
 
-saveRDS(object = df, file = paste0("result files_varying_B/pvalues_aCSS_B_", task_id, ".rds"))
+saveRDS(object = df, file = paste0("result files/pvalues_aCSS_B_", task_id, ".rds"))
